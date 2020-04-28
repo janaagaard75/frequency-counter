f3ad989c3395b9983974b96c736cd7222aeda780
Jan Aagaard <jan@aagaard.net>
2020-04-28 22:06:13 +0200

    Count number of entries in sitemap

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 class WordFrequencyCounter {
   constructor(private sitemapUrl: string) {}
 
-  public start() {
-    console.info(`Starting with sitemapUrl ${this.sitemapUrl}.`);
+  public async start() {
+    const response = await fetch(this.sitemapUrl)
+    const sitemapString = await response.text()
+
+    const parser = new DOMParser()
+    const sitemap = parser.parseFromString(sitemapString, "application/xml")
+
+    console.info(`Sitemap entries: ${sitemap.children[0].children.length}.`)
   }
 }
 
 function start(sitemapUrl: string) {
-  event?.preventDefault();
-  const counter = new WordFrequencyCounter(sitemapUrl);
-  counter.start();
+  event?.preventDefault()
+  const counter = new WordFrequencyCounter(sitemapUrl)
+  counter.start()
 }
