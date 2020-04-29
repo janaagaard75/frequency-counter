54fd934d6f3218423158c67bf6f73b4f4369ff22
Jan Aagaard <jaa@cvation.com>
2020-04-29 14:11:10 +0200

    Output word occurences

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined
+}
+
 class WordFrequencyCounter {
   constructor(private sitemapUrl: string) {}
 
@@ -13,11 +17,71 @@ class WordFrequencyCounter {
     const sitemapString = await response.text()
     const parser = new DOMParser()
     const sitemap = parser.parseFromString(sitemapString, "application/xml")
-    const pageUrls = Array.from(sitemap.querySelectorAll("loc")).map(
-      (loc) => loc.textContent
-    )
+    const pageUrls = Array.from(sitemap.querySelectorAll("loc"))
+      .map((loc) => loc.textContent)
+      .filter(notEmpty)
 
     outputElement.innerHTML = pageUrls.join("\n")
+
+    const fetchWordsTasks = pageUrls.map((pageUrl) => this.fetchWords(pageUrl))
+    const minimumWordLength = 5
+    const words = (await Promise.all(fetchWordsTasks))
+      .flat()
+      .filter((word) => word.length >= minimumWordLength)
+
+    const occurrencesObject = words.reduce<{
+      [index: string]: { word: string; count: number }
+    }>(
+      (accumulator, word) => (
+        (accumulator[word.toLowerCase()] = {
+          word: word,
+          count: (accumulator[word.toLowerCase()]?.count || 0) + 1,
+        }),
+        accumulator
+      ),
+      {}
+    )
+
+    const occurrencesArray: Array<{ word: string; count: number }> = []
+    for (var prop in occurrencesObject) {
+      if (occurrencesObject.hasOwnProperty(prop)) {
+        occurrencesArray.push(occurrencesObject[prop])
+      }
+    }
+
+    const sorted = occurrencesArray.sort(
+      (wordCountA, wordCountB) => wordCountB.count - wordCountA.count
+    )
+
+    outputElement.innerHTML += "\n"
+
+    sorted
+      .slice(0, 50)
+      .forEach(
+        (wordCount) =>
+          (outputElement.innerHTML += `\n${wordCount.word}: ${wordCount.count}`)
+      )
+  }
+
+  private async fetchWords(pageUrl: string): Promise<Array<string>> {
+    const response = await fetch(pageUrl)
+    const html = await response.text()
+    const parser = new DOMParser()
+    const document = parser.parseFromString(html, "text/html")
+    // TODO: Filter out scripts and CSS. Try with a positive list including only look at h* and p elements?
+    // TODO: Also look at the title and the meta description.
+    // TODO: Weight title, description and headers higher.
+    const content = document.querySelector("body")?.innerText
+    if (content === undefined) {
+      return []
+    }
+
+    const words = content
+      .replace(/[^a-zA-ZæøåÆØÅ0-9]/g, " ")
+      .split(" ")
+      .map((word) => word.trim())
+      .filter((word) => word !== "")
+    return words
   }
 }
 
