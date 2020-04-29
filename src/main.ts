a3b8dca1b79fc0ecd5bdbf7ab7b8ee0fd9be62b3
Jan Aagaard <jaa@cvation.com>
2020-04-29 08:45:02 +0200

    Clear output when restarting

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,22 @@ class WordFrequencyCounter {
   constructor(private sitemapUrl: string) {}
 
   public async start() {
+    const outputElement = document.getElementById("output")
+    if (outputElement === null) {
+      return
+    }
+
+    outputElement.innerHTML = "Fetching..."
+
     const response = await fetch(this.sitemapUrl)
     const sitemapString = await response.text()
-
     const parser = new DOMParser()
     const sitemap = parser.parseFromString(sitemapString, "application/xml")
-
     const urls = Array.from(sitemap.querySelectorAll("loc")).map(
       (loc) => loc.textContent
     )
 
-    const outputElement = document.getElementById("output")
-    if (outputElement !== null) {
-      outputElement.innerHTML = urls.join("\n")
-    }
+    outputElement.innerHTML = urls.join("\n")
   }
 }
 
